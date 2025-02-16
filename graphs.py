78dd3b39a12eaef170e72ca6f4f6dd9e130abdc1
AayushParab44 <aayush.s.parab3@gmail.com>
2025-02-17 00:52:46 +0530

    updated graphs practice file

--- graphs.py
+++ graphs.py
@@ -2,6 +2,9 @@ graph_x={'A':['B'],'B':['C','D'],'C':['E','B','A'],'D':['E'],'E':['D','C']}
 
 graph_x={'a':['c','b'],'b':['d'],'c':['e'],'d':['f'],'e':[],'f':[]} #abdfce
 
+graph_x={'a':['b','c'],'b':['d'],'c':['e'],'d':['f'],'e':[],'f':[]} #abdfce
+
+
 def dfs_graph(graph,root):
     stk=[root]
     while len(stk)!=0:
@@ -9,11 +12,16 @@ def dfs_graph(graph,root):
         print(popped)
         stk.extend(graph[popped])
 
-
+print('dfs using graph iteratively-')
 dfs_graph(graph_x,'a')
 
 print()
+print('dfs using graph recursively-')
+
 
 def dfs_rec(graph,root):
-    pass
+    print(root)
+    for i in graph[root]:
+        dfs_rec(graph,i)
 
+dfs_rec(graph_x,'a')
